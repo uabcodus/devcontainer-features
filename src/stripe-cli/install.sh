403b1f83fc43cc44e1d70eed79342de4eac164ee
Deividas Valiukas <deivis.valiukas@gmail.com>
2026-02-18 13:40:51 +0200

    Update stripe CLI install script to not use sudo

--- src/stripe-cli/install.sh
+++ src/stripe-cli/install.sh
@@ -23,10 +23,10 @@ apt-get -y update
 apt-get -y install --no-install-recommends curl ca-certificates gpg
 
 # Add Stripe CLI’s GPG key to the apt sources keyring
-curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | sudo tee /usr/share/keyrings/stripe.gpg
+curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | tee /usr/share/keyrings/stripe.gpg
 
 # Add CLI’s apt repository to the apt sources list
-echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | sudo tee -a /etc/apt/sources.list.d/stripe.list
+echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | tee -a /etc/apt/sources.list.d/stripe.list
 
 # Update the package list
 apt-get -y update
